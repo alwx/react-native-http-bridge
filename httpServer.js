271efbbf918d8f1ad982867c30a8be2bbe3cfe17
alwx <alwxndr@gmail.com>
2017-02-07 14:24:35 +0300

    fixes for event emmiter

--- httpServer.js
+++ httpServer.js
@@ -13,13 +13,12 @@ module.exports = {
             throw "Invalid server port specified. Port 80 is reserved.";
         }
 
-        Server.start(port, function () {
-            DeviceEventEmitter.addListener('httpServerResponseReceived', callback);
-        });
+        Server.start(port);
+        DeviceEventEmitter.addListener('httpServerResponseReceived', callback);
     },
 
-    // effectively pause the instance of the server
     stop: function () {
         Server.stop();
+        DeviceEventEmitter.removeListener('httpServerResponseReceived');
     }
 }
